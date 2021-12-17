bfd0404bcf3fac02701621fdf7e022713518d220
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-12-17 12:57:54 -0500

    User model created

--- models/User.js
+++ models/User.js
@@ -1,1 +1,40 @@
 const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: "Username is required",
+      trim: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: "Email is required",
+      match: [/.+@.+\..+/]
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought"
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Type.ObjectId,
+        ref: "User"
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true
+    },
+    id: false
+  }
+);
+
+UserSchema.virtual("friendCount").get(function() {
+  return this.friends.reduce((total, friend) => total + friend.replies.length + 1, 0)
+});
