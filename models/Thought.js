ce6635a28d76d898554b0114b7a874532567cef3
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-12-21 10:29:27 -0500

    reaction schema completed

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,34 @@
 const { Schema, model } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
 
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: "Text is required",
+      maxLength: 280
+    },
+    username: {
+      type: String,
+      required: "Username is required"
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+)
+
 const ThoughtSchema = new Schema(
   {
     thoughtText: {
@@ -16,7 +44,7 @@ const ThoughtSchema = new Schema(
     },
     username: {
       type: String,
-      required: "Username is required",
+      required: "Username is required"
     },
     reactions: [ReactionSchema]
   },
