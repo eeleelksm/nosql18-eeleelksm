779f562900c16af15be8679059aeec6f73b50f94
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-12-21 19:41:33 -0500

    added update and delete user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -44,8 +44,35 @@ const userController = {
     User.create(body)
     .then(dbUserData => res.json(dbUserData))
     .catch(err => res.status(400).json(err))
-  }
+  },
 
+  // update a user
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then(dbUserData => {
+        // if the user isn't found, send 404
+        if(!dbUserData) {
+          res.status(404).json({ message: "There isn't a user with this id."})
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
+
+  // delete a user
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+    .then(dbUserData => {
+      // if the user isn't found, send 404
+      if(!dbUserData) {
+        res.status(404).json({ message: "There isn't a user with this id."})
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => res.status(400).json(err));
+  }
 }
 
 module.exports = userController;
